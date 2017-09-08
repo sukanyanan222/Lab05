10608a5d8f8448e054256c259e95b123a7dc1738
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 15:45:20 +0700

    W05P11

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -10,6 +10,15 @@ namespace Labb5
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 20;
+            int c = add(a, b);
         }
+        private static int add(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
     }
+}
 }
