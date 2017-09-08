3e30fd94ed9d390aca1f97d62b13b102d822cd74
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 15:29:11 +0700

    W05P07

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -10,6 +10,21 @@ namespace Labb5
     {
         static void Main(string[] args)
         {
+            int a = 0;
+            try
+            {
+
+                Console.WriteLine(100 / a);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
