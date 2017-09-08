f4557f5489404e4459706724314c88d61029e1fe
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 15:41:27 +0700

    W05P10

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -10,6 +10,24 @@ namespace Labb5
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int value = 800000000;
+                checked // check for overflow
+                {
+                    int square = value * value;
+                    Console.WriteLine("{0} ^ 2 = {1}", value, square);
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
         }
     }
 }
