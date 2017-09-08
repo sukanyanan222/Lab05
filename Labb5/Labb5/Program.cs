bfd13b6d10f6a07818eec1a4bb27589e2d877682
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 17:27:59 +0700

    W05P25

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace Labb5
+namespace ConsoleApp2
 {
-    class Program
+    class BreakTest
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
