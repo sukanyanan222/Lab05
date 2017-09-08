b7843ef0a765bbdfecba03135dcac65b8012a518
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 16:28:35 +0700

    W05P17

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labb5
+using System.IO;
+public class switchLearning
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Input your grade (A, B, C, D or F) : ");
+        string gradeString = Console.ReadLine();
+        string message;
+        switch (gradeString.ToUpper())
         {
+            case "A":
+                message = "Excellent";
+                break;
+            case "B":
+                message = "Good";
+                break;
+            case "C":
+                message = "Cool";
+                break;
+            case "D":
+                message = "Try";
+                break;
+            case "F":
+                message = "Get out!!";
+                break;
+            default:
+                message = "Incorrect grade";
+                break;
         }
+        Console.WriteLine(message);
     }
 }
+
