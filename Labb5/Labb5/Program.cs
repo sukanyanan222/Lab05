5167248142128e537ada64b7a09014f079e24ea7
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 17:20:10 +0700

    W05P23

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace Labb5
+namespace ConsoleApp2
 {
-    class Program
+    class Multiply
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            for (int i = 1; i <= 12; i++)
+            {
+                for (int j = 1; j <= 12; j++)
+                {
+                    Console.Write("{0, 4}", i * j);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
