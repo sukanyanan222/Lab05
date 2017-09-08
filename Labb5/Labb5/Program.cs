c83f6555f46cd76857d75503d4ecd518840fc011
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 17:33:03 +0700

    W05P27

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labb5
+namespace ConsoleApp2
 {
-    class Program
+    class ContinueTest
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i < 9)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
