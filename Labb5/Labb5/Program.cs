1b90a312015bb7ec96830b98f31c6dfdbb98e7d2
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 16:25:29 +0700

    W05P16

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labb5
+﻿
+using System;
+using System.IO;
+public class IfLearning
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        int point = 68;
+        if (point < 50)
+            Console.WriteLine("Grade F");
+        else if (point < 60)
+            Console.WriteLine("Grade D");
+        else if (point < 70)
+            Console.WriteLine("Grade C");
+        else if (point < 80)
+            Console.WriteLine("Grade B");
+        else
+            Console.WriteLine("Grade A");
     }
 }
