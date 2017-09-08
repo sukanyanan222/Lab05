99908351657e9053c8783948562297ce385a3a39
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 16:21:41 +0700

    W05P15

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labb5
+﻿
+using System;
+using System.IO;
+public class IfLearning
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        int a = 10;
+        int b = 20;
+        if (a == 10)
         {
+            if (b == 20)
+            {
+                Console.WriteLine("a = 10 and b = 20");
+            }
+            if (b != 20)
+            {
+                Console.WriteLine("a = 10 and b != 20");
+            }
         }
     }
 }
