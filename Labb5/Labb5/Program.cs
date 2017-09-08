b2154cb6e3d19270572752952809f0818701c663
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 16:19:04 +0700

    W05P14

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labb5
+﻿
+using System;
+using System.IO;
+public class IfLearning
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        int a = 2;
+        if (a == 2)
         {
+            Console.WriteLine("execute this line");
         }
+        else
+        {
+            Console.WriteLine("execute another line too");
+        }
+        Console.WriteLine("this line is always execute");
     }
 }
