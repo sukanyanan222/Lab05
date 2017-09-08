9c1d00cc93698b878c0b748c3edc00b2a112db97
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 15:27:02 +0700

    W05P06

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -10,6 +10,17 @@ namespace Labb5
     {
         static void Main(string[] args)
         {
+            object o2 = null;
+            try
+            {
+                int i2 = (int)o2;
+                Console.WriteLine("i2 = {0}", i2);
+            }
+            catch
+            {
+                Console.WriteLine("Error, null object assignment.");
+            }
+
         }
     }
 }
