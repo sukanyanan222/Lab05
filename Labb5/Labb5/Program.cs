ade474f1a6e418429c71be9fc9243f9bc60b0676
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 17:08:30 +0700

    W05P20

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Labb5
+namespace ConsoleApp2
 {
-    class Program
+    class DoTest
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("---- Begin of While statement.");
+            int n = 1;
+            while (n < 1)
+            {
+                Console.WriteLine("  Current value of n is {0}", n);
+                n++;
+            }
+            Console.WriteLine("---- End of While statement.");
+
+            Console.WriteLine("---- Begin of Do..While statement.");
+            n = 1;
+            do
+            {
+                Console.WriteLine("  Current value of n is {0}", n);
+                n++;
+            }
+            while (n < 1);
+            Console.WriteLine("---- End of Do..While statement.");
         }
     }
 }
