86a47733e863d47a11191487aaa6e8053f4e774c
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 16:11:23 +0700

    W05P13

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labb5
+﻿
+using System;
+using System.IO;
+public class IfLearning
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        int a = 2;
+        if (a == 2)
         {
+            Console.WriteLine("execute this line");
         }
+        if (a < 2)
+        {
+            Console.WriteLine("execute this line too");
+        }
+        Console.WriteLine("execute next line");
     }
 }
