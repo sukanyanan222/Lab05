fe26ab026e11202b53635b57d354743d3a37d9b9
Sukanya <Sukanyak21130@gmail.com>
2017-09-08 15:22:08 +0700

    W05P03

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -10,6 +10,36 @@ namespace Labb5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Coffee sizes: 1=Small 2=Medium 3=Large");
+            Console.Write("Please enter your selection: ");
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            int cost = 0;
+            switch (n)
+            {
+                case 1:
+                    cost += 25;
+                    break;
+                case 2:
+                    cost += 25;
+                    goto case 1;
+                case 3:
+                    cost += 50;
+                    goto case 1;
+                default:
+                    Console.WriteLine("Invalid selection.");
+                    break;
+            }
+            if (cost != 0)
+            {
+                Console.WriteLine("Please insert {0} Bath.", cost);
+            }
+            Console.WriteLine("Thank you for your business.");
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
         }
     }
 }
