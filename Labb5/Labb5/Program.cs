78fb37e5d2bbf2e8845830b288e72b29c15c9ff4
Sukanya <Sukanyak21130@gmail.com>
2017-09-14 17:31:42 +0700

    W05P29

--- Labb5/Labb5/Program.cs
+++ Labb5/Labb5/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labb5
+using System.IO;
+public class switchLearning
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Input day name : (sun, mon, tue, wed, thu, fri or sat) : ");
+        string dayString = Console.ReadLine();
+        string message;
+        switch (dayString.ToLower())
         {
+            case "sun":
+                message = "sun is Sunday, color Red";
+                break;
+            case "mon":
+                message = "mon is Monday, color Yellow";
+                break;
+            case "tue":
+                message = "tue is Tuesday, color Pink";
+                break;
+            case "wed":
+                message = "wed is Wednesday, color Green";
+                break;
+            case "thu":
+                message = "thu is Thursday, color Orange";
+                break;
+            case "fri":
+                message = "fri is Friday, color Blue";
+                break;
+            case "sat":
+                message = "sast is Saturday, color Purple";
+                break;
+            default:
+                message = "Please input day name : (sun, mon, tue, wed, thu, fri or sat)";
+                break;
         }
+        Console.WriteLine(message);
     }
 }
